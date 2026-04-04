1e1f9707445cac49df014c7eea702eab55301dbb
OVIYA <oviyass1226@gmail.com>
2026-04-04 16:14:53 +0530

    Update axios.js to connect to live backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
+// Live backend URL
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://costtracking-q58t.onrender.com',
 });
 
-api.interceptors.request.use((config) => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.token) {
-        config.headers.Authorization = `Bearer ${user.token}`;
-    }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
-});
+api.interceptors.request.use(
+    (config) => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (user && user.token) {
+            config.headers.Authorization = `Bearer ${user.token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default api;
